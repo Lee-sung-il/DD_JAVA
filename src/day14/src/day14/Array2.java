6456c4e53453c36483b89cb9b37fc552efad75be
LeeSungil <wj132612@naver.com>
2021-03-10 01:00:10 +0900

    Commit

---
+++ src/day14/src/day14/Array2.java
@@ -0,0 +1,12 @@
+package day14.src.day14;
+
+public class Array2 {
+    public static void main(String[] args) {
+        // 5개의 변수 묶는 1차원 배열 x2 = 2차원 배열
+        int ar1[] = new int[5];
+        int ar2[] = new int[5];
+
+        //자료형 배열명[][] = new int [묶음 크기][변수 갯수];
+        int ar3[][] = new int[2][5];
+    }
+}
